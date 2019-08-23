a2a45a73bcccff5ed9342a9abe0975450e13d07c
JunyaHB <a1933jh@aiit.ac.jp>
2019-08-23 18:22:57 +0900

    complete

---
+++ dist/script.js
@@ -0,0 +1,25 @@
+"use strict";
+
+$(function () {
+  $('.social-icon').hover(function () {
+    $(this).animate({
+      'font-size': '30px'
+    }, 300);
+  }, function () {
+    $(this).animate({
+      'font-size': '24px'
+    }, 300);
+  });
+  $("#top-btn").click(function () {
+    $('html, body').animate({
+      scrollTop: 0
+    }, 500);
+  });
+  $('header a').click(function () {
+    var id = $(this).attr('href');
+    var position = $(id).offset().top;
+    $('html, body').animate({
+      'scrollTop': position
+    }, 500);
+  });
+});
